08a4a3905d5fe8d11bd8b17dd7d946756824fcac
Nisa <nisa8610@gmail.com>
2025-08-20 10:37:11 +0530

    replace even with 0 and odd with 1

---
+++ Leetcode3467/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = { 4, 3, 2, 1 };
+
+        Console.WriteLine(string.Join(", ", TransformArray(nums)));
+    }
+    static int[] TransformArray(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                nums[i] = 0;
+            }
+            else
+            {
+                nums[i] = 1;
+            }
+        }
+        int temp;
+        for (int j = 0; j < nums.Length; j++)
+        {
+            for (int k = j + 1; k < nums.Length; k++)
+            {
+                if (nums[j] > nums[k])
+                {
+                    temp = nums[j];
+                    nums[j] = nums[k];
+                    nums[k] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
+
+
+
+
+
+
