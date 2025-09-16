0299550a802658f0c7f042ba25702ab5ad350627
Nisa <nisa8610@gmail.com>
2025-09-16 10:07:08 +0530

    Final Value of Variable After Performing Operations

---
+++ Leetcode2011/FinalValue.js
@@ -0,0 +1,12 @@
+var finalValueAfterOperations = function(operations) {
+    let finalValueOfOperations=0;
+for(let i=0;i<operations.length;i++){
+  if(operations[i]==="X++" || operations[i]==="++X"){
+      finalValueOfOperations++;
+  }
+  else if(operations[i]==="X--" || operations[i]==="--X"){
+      finalValueOfOperations--;
+  }
+}
+return finalValueOfOperations;
+};
