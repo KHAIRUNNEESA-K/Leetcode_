901cbd4bf00f004a50a3ba02398bb3fc8f487e8d
Nisa <nisa8610@gmail.com>
2025-10-04 14:42:38 +0530

    Final Array State After K Multiplication Operations I

---
+++ Leetcode3264/getFinalArray.js
@@ -0,0 +1,13 @@
+var getFinalState = function(nums, k, multiplier) {
+    while(k > 0){
+    let minIndex = 0;
+    for(let i=1; i<nums.length; i++){
+        if(nums[i] < nums[minIndex]){
+            minIndex = i;
+        }
+    }
+    nums[minIndex] = nums[minIndex] * multiplier;
+    k--;
+}
+return nums;
+};
