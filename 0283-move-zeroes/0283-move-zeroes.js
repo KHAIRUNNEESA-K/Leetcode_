d8100edc73967db923f6e10dc02ba9f709bf7121
Nisa <nisa8610@gmail.com>
2025-10-21 18:35:17 +0530

    Time: 1 ms (76.53%), Space: 60.6 MB (29.57%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -2,16 +2,17 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-    let temp=0
-for(let i=0;i<nums.length;i++){
-    for(let j=i+1;j<nums.length;j++){
-        if(nums[i] === 0 && nums[j] != 0){
-            temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
+var moveZeroes = function (nums) {
+    let i = 0
+    for (let num of nums) {
+        if (num != 0) {
+            nums[i] = num
+            i++
         }
     }
-}
-return nums;
+    while (i < nums.length) {
+        nums[i] = 0;
+        i++
+    }
+    return nums;
 };
