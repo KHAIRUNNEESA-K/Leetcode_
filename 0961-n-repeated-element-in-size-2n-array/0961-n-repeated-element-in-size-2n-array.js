034b39b8e8c9cc77b69234d9a012810b31bc60b3
Nisa <nisa8610@gmail.com>
2026-01-02 13:04:30 +0530

    Time: 483 ms (5.04%), Space: 58.8 MB (42.86%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function (nums) {
+    let element = 0
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] == nums[j]) {
+                element = nums[i]
+            }
+        }
+    }
+    return element;
+};
